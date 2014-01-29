0f994bfe784755cdc875ad2691bc9ca2030cd4dd
Fenixp <david.orolin@gmail.com>
2014-01-29 18:59:02 +0100

    Hele, normálně ti takhle commituju a co nevidím - šít se stal!

--- GitPokusAplikace/Program.cs
+++ GitPokusAplikace/Program.cs
@@ -10,8 +10,16 @@ namespace GitPokusAplikace
     {
         static void Main(string[] args)
         {
-            System.Console.WriteLine("Sbohem zly svete");
+            int hello = 5;
+            int dude = 6;
+            int vysledek = hello + dude;
+            System.Console.WriteLine("Sbohem zly svete, soucet helloduda je " + vysledek + " a Hello dne je: " + prom(10));
             System.Console.ReadKey();
+        }
+
+        static int prom(int hello)
+        {
+            return (hello * hello);
         }
     }
 }
